1784b1b3350e2555058e64dad3fe6b25047a2eb2
François Gombault <francois.gombault@posteo.de>
2023-06-26 20:37:44 +0200

    only import 1/10 modifiers, based on world seed

--- files/append_enchantments.lua
+++ files/append_enchantments.lua
@@ -1,22 +1,25 @@
-if(actions == nil)then
+if (actions == nil) then
 	reading_name = true
 	dofile("data/scripts/gun/gun_actions.lua")
 end
-for k, v in pairs(actions)do
-	if(v.type == ACTION_TYPE_MODIFIER)then
+local seed = MagicNumbersGetValue("WORLD_SEED")
+for k, v in pairs(actions) do
+	local pass = ((seed + k) % 10 == 0)
+	if (v.type == ACTION_TYPE_MODIFIER and pass) then
 		pretty_print = dofile("mods/evaisa.enchantments/files/scripts/pretty_print.lua")
 
 		--pretty_print.table(v)
 
 		table.insert(enchantments, {
-			id = v.id.."_enchantment",
+			id = v.id .. "_enchantment",
 			name = v.name,
 			description = v.description,
 			icon = v.sprite,
 			author = "???",
-			valid_spell_types = {ACTION_TYPE_PROJECTILE, ACTION_TYPE_STATIC_PROJECTILE, ACTION_TYPE_MODIFIER, ACTION_TYPE_DRAW_MANY, ACTION_TYPE_PASSIVE},
+			valid_spell_types = { ACTION_TYPE_PROJECTILE, ACTION_TYPE_STATIC_PROJECTILE, ACTION_TYPE_MODIFIER,
+				ACTION_TYPE_DRAW_MANY, ACTION_TYPE_PASSIVE },
 			is_stackable = true,
-			card_extra_entities = {v.custom_xml_file},
+			card_extra_entities = { v.custom_xml_file },
 			weight = 1.0,
 			check = function(action)
 				return true
@@ -30,11 +33,11 @@ for k, v in pairs(actions)do
 				draw_actions = old_modifier_enchantment_data
 
 				orig(recursion_level, iteration)
-				
 			end,
-			projectile_spawn = function(projectile_entity) 
+			projectile_spawn = function(projectile_entity)
 
 			end
 		})
 	end
 end
+
