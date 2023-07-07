32e8cb38be1e69c68fba9fd2092a065cdc994f22
François Gombault <francois.gombault@posteo.de>
2023-07-07 17:12:03 +0200

    fix enchantments fizzling if they vanish from the global list

--- files/append_enchantments.lua
+++ files/append_enchantments.lua
@@ -3,31 +3,8 @@ if (actions == nil) then
 	dofile("data/scripts/gun/gun_actions.lua")
 end
 
-local function create_xorshift_prng()
-	local g = {
-		state = 0xdeadbeef,
-		seed = function(self, num)
-			if num then
-				self.state = tonumber(num)
-			end
-		end,
-		next_int = function(self)
-			local x = self.state
-			x = bit.bxor(x, bit.lshift(x, 13))
-			x = bit.bxor(x, bit.rshift(x, 17))
-			x = bit.bxor(x, bit.lshift(x, 5))
-			self.state = x
-			return x
-		end,
-	}
-	return g
-end
-local rng = create_xorshift_prng()
-rng:seed(StatsGetValue("gold"))
-
 for k, v in pairs(actions) do
-	local pass = ((rng:next_int()) % 10 == 0)
-	-- if v.name contains "Eternal" then don't pass
+	local pass = true
 	if string.find(v.name, "Eternal") then
 		pass = false
 	end
@@ -45,7 +22,7 @@ for k, v in pairs(actions) do
 			valid_spell_types = { ACTION_TYPE_PROJECTILE, ACTION_TYPE_STATIC_PROJECTILE },
 			is_stackable = true,
 			card_extra_entities = { v.custom_xml_file },
-			weight = 1.0,
+			weight = 0.1,
 			check = function(action)
 				return true
 			end,
